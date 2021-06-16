0b65296b67233f05553bc4a21732c498792b9390
adavies <arielpdavies@gmail.com>
2021-06-16 14:47:09 -0400

    toggled

---
+++ skeleton-6/frontend/follow_toggle.js
@@ -0,0 +1,12 @@
+const { Module } = require("webpack");
+
+class FollowToggle{
+    constructor(el) {
+        this.$el = $(el);
+        this.userId = this.$el.data("user-id");
+        this.followState = this.$el.data("initial-follow-state");
+    }
+
+}
+
+module.exports = FollowToggle;
